7058995641217638b2ab4f6ca688c92e19a23153
Anand shukla <131698705+Aanandshukla02@users.noreply.github.com>
2025-08-22 23:33:09 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-2l0ohjaq8sdnojh2.us.auth0.com"   // apna domain
+    clientId="JK6ThZFj9hzmJAZ7ghANbwzUIbfXli7p" // apna clientId
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
